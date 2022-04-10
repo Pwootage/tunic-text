074049333a633b585c0c0705ef8bd54eaf0afa04
Pwootage <pwootage@gmail.com>
2022-04-10 16:52:05 -0600

    Remove unused stuff

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,18 @@
 <template>
-  <header>
-    <div>
-      <TunicText :phrase="phrase"/>
-      <br/>
-      <TunicText :phrase="phrase" :mode="'hex'"/>
-      <TunicText :phrase="phrase" :letterSize="40"/>
-      <TunicText :phrase="phrase" :letterSize="10"/>
-      <TunicTextEditor/>
+  <div>
+    <TunicText :phrase="phrase"/>
+    <!--      <TunicText :phrase="phrase" :mode="'hex'"/>-->
+    <!--      <TunicText :phrase="phrase" :letterSize="40"/>-->
+    <!--      <TunicText :phrase="phrase" :letterSize="10"/>-->
+    <TunicTextEditor/>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+    <!--      <nav>-->
+    <!--        <RouterLink to="/">Home</RouterLink>-->
+    <!--        <RouterLink to="/about">About</RouterLink>-->
+    <!--      </nav>-->
+  </div>
 
-  <RouterView/>
+  <!--  <RouterView/>-->
 </template>
 
 <script setup lang="ts">
@@ -31,22 +28,17 @@ const phrase = ref(TEST_PHRASE);
 <style lang="scss">
 @import 'base';
 
+body {
+  display: flex;
+  place-items: center;
+}
+
 #app {
   max-width: 1280px;
   margin: 0 auto;
   padding: 2rem;
 
   font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
 }
 
 a,
@@ -59,69 +51,6 @@ a,
 @media (hover: hover) {
   a:hover {
     background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
   }
 }
 </style>
